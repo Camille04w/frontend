3551fa872c6717f9cf16b45efaf590c33385f7b6
Camille04w <vitoriacamille603@gmail.com>
2024-07-03 14:55:14 -0300

    save6

--- src/pages/cadastro.tsx
+++ src/pages/cadastro.tsx
@@ -1,11 +1,33 @@
-import styles from '../../styles/home.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
+import Logo from '../img/logo.png';
+import styles from '../../styles/cadastro.module.scss';
 
 
 export default function Cadastro() {
     return (
         <>
-            <div className={styles.body}>
-                <h1>Cadastro</h1>
+            <div className={styles.all}>
+                <div className={styles.body}>
+                    <Head>
+                        <title>Biblioteca Next</title>
+                        <link rel="stylesheet" href="/styles/globals.scss" />
+                    </Head>
+                    <div className={styles.container}>
+                        <div className={styles.block}>
+                            <div className={styles.logo}>
+                                <Image src={Logo} alt="Logo da Biblioteca Next" layout="intrinsic" />
+                            </div>
+                            <form className={styles.form}>
+                                <input type="email" placeholder="Digite seu email" className={styles.input} />
+                                <input type="password" placeholder="Crie uma senha" className={styles.input} />
+                                <input type="password" placeholder="Digite a senha novamente" className={styles.input} />
+                                <button type="submit" className={styles.button}>Entrar</button>
+                            </form>
+                            <a href='/login' className={styles.link}>Já tem uma conta?</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
 
